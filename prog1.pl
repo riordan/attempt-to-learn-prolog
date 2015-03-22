47e87ec068cd5184a983ddaac0f0cca98ae39a3d
David Riordan <david.riordan@gmail.com>
2015-03-22 17:36:10 -0400

    just the book

---
+++ prog1.pl
@@ -0,0 +1,12 @@
+human(david).
+human(john).
+human(suzie).
+human(eliza).
+man(david).
+man(john).
+woman(suzie).
+woman(eliza).
+parent(david, john).
+parent(john, eliza).
+parent(suzie, eliza).
+
